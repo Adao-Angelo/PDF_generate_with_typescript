1ff5d7724a51d22f4372f1e4700a0b82bdb8d2fd
adaobegginer <adaobegginer@gmail.com>
2023-12-15 04:50:27 +0100

    first commit

---
+++ src/generatePDF.ts
@@ -0,0 +1,18 @@
+import {jsPDF} from "jspdf";
+interface IRequet  {
+    content:string;
+    name:string;
+}
+ class GeneratePDF {
+    constructor(){
+
+    }
+
+    handle({content , name}:IRequet){
+        const docPDF = new jsPDF();
+        return docPDF.text(content , 10 , 10 );
+    }
+}
+
+const generatePDF =  new GeneratePDF() 
+export {generatePDF}
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { request, response } from "express";
+import { generatePDF } from "./generatePDF";
+
+const port = 2000;
+const  app = express();
+
+app.get("/pdf" , (request , response)=>{    
+
+    const dt = generatePDF.handle({content:"welcome", name:"text.pdf"})
+    response.send(dt)
+})
+
+
+app.listen(port , ()=>{
+    console.log("server is on")
+})
+
