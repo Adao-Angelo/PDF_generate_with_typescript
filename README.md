cd3ddfe3c05b1566ef6ab0bf41e1f6d5ac95663b
adaobegginer <adaobegginer@gmail.com>
2023-12-15 15:29:26 +0100

    work tha src parte

--- README.md
+++
@@ -1,5 +0,0 @@
-  res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Length', fileSize);
-  res.setHeader('Content-Disposition', 'attachment; filename=file.pdf');
-
-  stream.pipe(res);
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,36 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, request } from "express";
 import { generatePDF } from "./generatePDF";
 import fs from "fs";
-import path from "path"
+import path from "path";
+
 
 const port = 2000;
-const  app = express();
-
-
-app.get('/pdf', (req, res) => {
-
-    generatePDF.handle({content:"welcome"  , name:"text,pdf"})
-    
-    const filePath = 'C:/Users/PC/Desktop/adão apps/jspdf/PDFs/text.pdf';
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('An error occurred while reading the file.');
-        return;
-      }
-      res.contentType('application/pdf');
-      res.send(data);
-    });
+const app = express();
+app.use(express.json())
+app.get("/pdf/:name", (req, res) => {
+  const name = `${req.params.name}.pdf` ;
+  const filePath = path.join(__dirname, "..", "PDFs", `${name}`);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("An error occurred while reading the file.");
+      return;
+    }
+    res.contentType("application/pdf");
+    res.send(data);
   });
-app.listen(port , ()=>{
-    console.log("server is on")
+});
+app.listen(port, () => {
+  console.log("server is on");
+});
+
+app.post("/pdf" , (req , res)=>{
+  const { content , name} = req.body;
+  generatePDF.handle({content:content , name:name})
+
+
+  res.status(201).json({message:"sucesse"})
 })
+
+
 
